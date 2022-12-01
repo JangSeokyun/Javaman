40767b3daea61dd4f5451beaff594afd0319c14c
JangSeokyun <jangsy0623@naver.com>
2022-12-01 20:58:42 +0900

    12/01

--- src/UI/MainPanel.java
+++ src/UI/MainPanel.java
@@ -5,54 +5,51 @@ import javax.swing.*;
 public class MainPanel extends JFrame {
 	public MainPanel(JFrame frame) {
 		Container contentPane = frame.getContentPane();
-		contentPane.setBackground(new Color(0xECD8C8));		// ��� �� ĥ�ϱ�
-		contentPane.setLayout(null);						// �׸��� ���̾ƿ� - ��ġ�� ����
+		contentPane.setBackground(new Color(0xECD8C8));		
+		contentPane.setLayout(null);						
 		
 		/*-------------------------------곰 모드 선택 버튼---------------------------------*/
 		JButton BearModeBtn = new JButton("곰 모드");
-		BearModeBtn.setSize(150, 40);						// ���۹�ư ũ��
-		BearModeBtn.setLocation(275, 300); 				// ���۹�ư ��ġ
-		BearModeBtn.setForeground(Color.WHITE);			// ���۹�ư �۾� ����
-		BearModeBtn.setBackground(new Color(0x013a63));	// ���۹�ư ����
-		BearModeBtn.setFont(new Font("Neo둥근모 Pro 보통", Font.PLAIN, 20));	// ���۹�ư ��Ʈ
-		contentPane.add(BearModeBtn); // ���۹�ư �ޱ�
+		BearModeBtn.setSize(150, 40);					
+		BearModeBtn.setLocation(275, 300); 			
+		BearModeBtn.setForeground(Color.WHITE);			
+		BearModeBtn.setBackground(new Color(0x013a63));	
+		BearModeBtn.setFont(new Font("Neo둥근모 Pro 보통", Font.PLAIN, 20));	
+		contentPane.add(BearModeBtn); 
 		
 		/*---------------------------------물고기 모드 선택 버튼----------------------------------*/
 		JButton FishModeBtn = new JButton("물고기 모드");
-		FishModeBtn.setSize(150, 40);						// ��ŷ��ư ũ��
-		FishModeBtn.setLocation(450, 300);					// ��ŷ��ư ��ġ
-		FishModeBtn.setForeground(Color.WHITE);				// ��ŷ��ư �۾� ����
-		FishModeBtn.setBackground(new Color(0x5e3023));		// ��ŷ��ư ����
-		FishModeBtn.setFont(new Font("Neo둥근모 Pro 보통", Font.PLAIN, 20));	// ��ŷ��ư ��Ʈ
-		contentPane.add(FishModeBtn); // ��ŷ��ư �ޱ�
+		FishModeBtn.setSize(150, 40);				
+		FishModeBtn.setLocation(450, 300);				
+		FishModeBtn.setForeground(Color.WHITE);				
+		FishModeBtn.setBackground(new Color(0x5e3023));		
+		FishModeBtn.setFont(new Font("Neo둥근모 Pro 보통", Font.PLAIN, 20));
+		contentPane.add(FishModeBtn); 
 		/*-------------------------------무한 포식 모드 선택 버튼---------------------------------*/
 		JButton InfiniteModeBtn = new JButton("무한 포식 모드");
-		InfiniteModeBtn.setSize(150, 40);						// ���۹�ư ũ��
-		InfiniteModeBtn.setLocation(100, 300); 				// ���۹�ư ��ġ
-		InfiniteModeBtn.setForeground(Color.WHITE);			// ���۹�ư �۾� ����
-		InfiniteModeBtn.setBackground(new Color(0x013a63));	// ���۹�ư ����
-		InfiniteModeBtn.setFont(new Font("Neo둥근모 Pro 보통", Font.PLAIN, 20));	// ���۹�ư ��Ʈ
-		contentPane.add(InfiniteModeBtn); // ���۹�ư �ޱ�
+		InfiniteModeBtn.setSize(150, 40);					
+		InfiniteModeBtn.setLocation(100, 300); 				
+		InfiniteModeBtn.setForeground(Color.WHITE);			
+		InfiniteModeBtn.setBackground(new Color(0x013a63));	
+		InfiniteModeBtn.setFont(new Font("Neo둥근모 Pro 보통", Font.PLAIN, 20));
+		contentPane.add(InfiniteModeBtn);
 		
-		/*-----------------------------------����-----------------------------------*/
 		JLabel Title = new JLabel("Kuma-Gotchi", SwingConstants.CENTER);
 		Title.setBounds(210, 60, 274, 50);
 		Title.setFont(new Font("Neo둥근모 Pro 보통", Font.BOLD, 40));
 		Title.setForeground(Color.WHITE);
 		contentPane.add(Title);
-		/*-----------------------------------����-----------------------------------*/
+		
 		JLabel ModeTitle = new JLabel("모드 선택", SwingConstants.CENTER);
 		Title.setBounds(210, 60, 274, 50);
 		Title.setFont(new Font("Neo둥근모 Pro 보통", Font.BOLD, 40));
 		Title.setForeground(Color.WHITE);
 		contentPane.add(Title);
 		
-		/*-----------------------------------����-----------------------------------*/
 		ImageIcon Bear = new ImageIcon("images/mainbear.png");
 		Image img = Bear.getImage();
 		Image changeImg = img.getScaledInstance(350,350,Image.SCALE_SMOOTH);
 		ImageIcon changeIcon = new ImageIcon(changeImg);
-		// �� �ڵ�� ���� ũ�� ���� ������, ���� �ٲٸ� �ʿ�X
 		
 		JLabel MainBear = new JLabel(changeIcon);
 		MainBear.setBounds(175, 130, 350, 350);
